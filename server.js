94ca571b6916b1f4dc03e15fa3569823054204c0
Salman Lania <78688636+salmanlania@users.noreply.github.com>
2024-10-22 16:27:08 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,18 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors({
-  origin: '*'
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  
+  // Handle preflight request
+  if (req.method === 'OPTIONS') {
+    return res.status(200).json({});
+  }
+
+  next();
+});
 
 app.use(bodyParser.json());
 
